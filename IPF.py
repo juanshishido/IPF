eb4bb8dc2fe82a44eae58319e66b5735627ae681
juanshishido <juanshishido@cal.berkeley.edu>
2015-03-13 20:54:15 -0700

    Added optional seed_values input.

--- IPF.py
+++ IPF.py
@@ -1,31 +1,42 @@
-import numpy as numpy
+import numpy as np
+import os.path
 
 
-def IPF(row_totals, col_totals, random_fill=False):
-	"""
-	Take two inputs: row and column totals.
-	The inputs must be NumPy Arrays whose sums are equal.
-	"""
+def IPF(row_totals, col_totals, seed_values=None, random_fill=False):
+    """
+    Take two inputs: row and column totals.
+    The inputs must be NumPy Arrays whose sums are equal.
+    
+    Optional input: seed_values (CSV only).
 
-	# Conditions
-	assert type(row_totals) == np.ndarray
-	assert type(col_totals) == np.ndarray
-	assert sum(row_totals) == sum(col_totals)
-	assert random_fill in (True, False)
+    Option: random_fill.
+    """
 
-	# Initialize the matrix
-	if random_fill == False:
-		matrix = np.ones((len(row_totals), len(col_totals)))
-	else:
-		matrix = np.random.rand(len(row_totals),len(col_totals))
+    # Initial conditions
+    assert type(row_totals) == np.ndarray
+    assert type(col_totals) == np.ndarray
+    assert sum(row_totals) == sum(col_totals)
+    assert random_fill in (True, False)
 
-	# Row update
-	row_scalars = matrix.sum(axis=1) / row_totals
-	matrix = (matrix.T / row_scalars).T
+    # Initialize the matrix
+    if seed_values != None:
+    	assert os.path.isfile(seed_values)
+        matrix = np.loadtxt(seed_values, delimiter=',')
+        assert matrix.shape[0] == len(row_totals)
+        assert matrix.shape[1] == len(col_totals)
+    elif seed_values == None:
+        if random_fill == False:
+            matrix = np.ones((len(row_totals), len(col_totals)))
+        else:
+            matrix = np.random.rand(len(row_totals),len(col_totals))
 
-	# Column update
-	col_scalars = matrix.sum(axis=0) / col_totals
-	matrix = (matrix / col_scalars)
+    # Row update
+    row_scalars = matrix.sum(axis=1) / row_totals
+    matrix = (matrix.T / row_scalars).T
 
-	# Return
-	return matrix
+    # Column update
+    col_scalars = matrix.sum(axis=0) / col_totals
+    matrix = (matrix / col_scalars)
+
+    # Return
+    return matrix
